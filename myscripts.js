fbf2308f52ceb524fa2e310033822cf0726a158d
saitorashi <thilleroberto@gmail.com>
2020-07-03 23:50:59 +0900

    sdga

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,5 @@
 function myFunction() {
     var popup = document.getElementById("myPopup");
-     var popup = document.getElementById("myPopupImage");
     popup.classList.toggle("show");
    
     
