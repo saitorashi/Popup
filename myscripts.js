7d249fa327e5fe718464fb2e46dc9087a09680de
saitorashi <thilleroberto@gmail.com>
2020-07-03 23:47:40 +0900

    gsaj;g

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,9 @@
 function myFunction() {
     var popup = document.getElementById("myPopup");
+     var popup = document.getElementById("myPopupImage");
     popup.classList.toggle("show");
+   
+    
   }
+  
   
